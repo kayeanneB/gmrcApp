9d059713dece7bd61a527bff5faab7bbdf3ffbad
steffibonifacio <steffibonifacio@gmail.com>
2023-10-23 23:45:07 +0800

    [#86ctthx40] created initial colors_config file

---
+++ app/src/scaffold/colors_config.ts
@@ -0,0 +1,14 @@
+export const defaults = {
+    black: "#000000",
+    white: "#FFFFFF"
+}
+
+export const primaryColors = {
+    ...defaults,
+    green1: "#347B38",
+    green2: "#68966C",
+    green3: "#9FD4A3",
+    green4: "#DDF2D1",
+    green5: "#CDEBC5",
+} as const
+
