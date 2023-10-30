146d9be9ee3fc85d02a921c6f8537cf9271a21ac
steffibonifacio <steffibonifacio@gmail.com>
2023-10-30 15:01:50 +0800

    #86ctqv0c7 | added base code for Button component

--- app/src/components/Button/index.tsx
+++ app/src/components/Button/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import { TouchableOpacity } from "react-native";
+import Text from "../Text"
 
 export class Button extends React.Component<any, any>{
+  state = {
+    pressed: false
+  }
 
+  resetPressedState = () => {
+    this.setState({
+      pressed: false,
+    });
+  };
+
+  render(){
+    return <TouchableOpacity onPress={this.props.onPress} style={this.props.style}>
+      <Text>{this.props.title}</Text>
+    </TouchableOpacity>
+  }
 }
+
+export default Button
--- app/src/screens/HomeScreen/index.tsx
+++ app/src/screens/HomeScreen/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, ScrollView, Text } from "react-native";
 import { Styles } from "./style";
+import Button from "../../components/Button";
 
 class HomeScreen extends React.Component<any, any>{
   render(){
     return <ScrollView contentContainerStyle={Styles.scrollContainer}>
       <Text>HOME</Text>
+      <Button onPress={this.props.navigation.navigate()}>GO To Samples</Button>
     </ScrollView>
   }
 }
